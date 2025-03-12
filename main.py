005d43fa725c1839d36d50a6f5c37fbbf512fe32
Tobeker <t.wendenburg@yahoo.de>
2025-03-12 17:17:09 +0100

    add Player

--- main.py
+++ main.py
@@ -2,18 +2,24 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-
+from player import *
 from constants import *
 
 def main():
     pygame.init()
+    track_time = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
+        dt = track_time.tick(60) / 1000
     #print("Starting Asteroids!")
     #print(f"Screen width: {SCREEN_WIDTH}")
     #print(f"Screen height: {SCREEN_HEIGHT}")
